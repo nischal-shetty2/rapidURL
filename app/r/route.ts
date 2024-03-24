88fab7bdd75c31d3152f1600eee46d115a656ab0
Nischal Shetty <nischal2004coc@gmail.com>
2024-03-24 22:29:07 +0530

    fixed get endpoint url

--- app/r/route.ts
+++ app/r/route.ts
@@ -4,7 +4,7 @@ import Url from "../models/UrlSchema";
 
 export async function GET(req: NextRequest) {
   const fullUrl = req.nextUrl.href;
-  const urlId = fullUrl.split("?")[1];
+  const urlId = fullUrl.split("?")[1].split("=")[0];
   try {
     await dbConnect();
     const url = await Url.findOne({
@@ -19,7 +19,7 @@ export async function GET(req: NextRequest) {
       );
       return NextResponse.redirect(url.origUrl);
     } else {
-      return NextResponse.json({ status: 404, msg: fullUrl });
+      return NextResponse.json({ status: 404, msg: fullUrl + " doesnt exist" });
     }
   } catch (err) {
     return NextResponse.json({ status: 404, error: err });
