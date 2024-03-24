dae53a0ecfcda4e249ee5428387b35a8b033357d
Nischal Shetty <nischal2004coc@gmail.com>
2024-03-24 18:59:34 +0530

    removed from try catch

--- app/r/route.ts
+++ app/r/route.ts
@@ -3,9 +3,9 @@ import dbConnect from "../lib/dbConnect";
 import Url from "../models/UrlSchema";
 
 export async function GET(req: NextRequest) {
+  const fullUrl = req.nextUrl.href;
+  const urlId = fullUrl.split("?")[1];
   try {
-    const fullUrl = req.nextUrl.href;
-    const urlId = fullUrl.split("?")[1];
     await dbConnect();
     const url = await Url.findOne({
       urlId,
@@ -18,7 +18,9 @@ export async function GET(req: NextRequest) {
         { $inc: { clicks: 1 } }
       );
       return NextResponse.redirect(url.origUrl);
-    } else return NextResponse.json({ status: 404, msg: "Not found" });
+    } else {
+      return NextResponse.json({ status: 404, msg: "Not found" });
+    }
   } catch (err) {
     return NextResponse.json({ status: 404, error: err });
   }
